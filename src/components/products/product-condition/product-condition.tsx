26ce2285174d52a2e6d49b0f98a55cbc66ace6df
Jose Agapito Ramirez <agapox@gmail.com>
2022-05-23 02:03:35 -0400

    feature: product-condition component UI

---
+++ src/components/products/product-condition/product-condition.tsx
@@ -0,0 +1,11 @@
+import './product-condition.css';
+
+const ProductCondition = ({condition}: {condition: string}) => {
+    return (
+        condition === 'new' ? <span 
+            className="condition new">Nuevo</span> : <span 
+            className="condition used">Usado</span>
+    )
+}
+
+export default ProductCondition;
