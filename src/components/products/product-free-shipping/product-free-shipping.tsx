6c8399c97a441b8f204b3f94d81605c644b3b976
Jose Agapito Ramirez <agapox@gmail.com>
2022-05-23 02:04:41 -0400

    feature: product-free-shipping component UI

---
+++ src/components/products/product-free-shipping/product-free-shipping.tsx
@@ -0,0 +1,9 @@
+import './product-free-shipping.css';
+
+const ProductFreeShipping = ({ freeShipping }: {freeShipping: boolean}) => {
+    return (
+        freeShipping ? <p className="free-shipping">Envío gratis <i className="fa-solid fa-truck-fast"></i></p> : null
+    )
+}
+
+export default ProductFreeShipping;
